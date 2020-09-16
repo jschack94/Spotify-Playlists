32c30641fc4370442bbd511bacb2599cb5aefdef
Jonathan <jschack94@gmail.com>
2020-09-16 14:47:29 -0500

    users controller setup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+    # Create new user
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to user_albums_path(@user)
+      else
+        render :index
+      end
+    end
+  
+    # Show 
+    def show
+      @user = User.find(params[:id])
+      render json: @user
+    end
+  
+  private
+  
+    # Accpetable user params
+    def user_params
+      params.require(:user).permit(:username, :email)
+    end
+  
+  end
