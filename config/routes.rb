6d1d8e678ca7717fcb698727f36e42bfbe205836
Jonathan <jschack94@gmail.com>
2020-12-01 16:41:36 -0600

    new

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   post '/users/:id/albums/search' => 'albums#search', as: 'album_search'
   get '/auth/spotify/callback' => 'sessions#create'
   post '/sessions', to: 'sessions#create'
-  get '/logou', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 end
